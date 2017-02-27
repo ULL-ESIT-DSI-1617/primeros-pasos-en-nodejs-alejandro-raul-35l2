d2a590d4ac9808982bd1c3a21ae669faef6e9993
AlejandrDiaz <alu0100761948@ull.edu.es>
2017-02-27 19:55:20 +0000

    Reescribiendo generate-gitbook a javascript #3

---
+++ scripts/generate-gitbook.js
@@ -0,0 +1,12 @@
+generateGitbook();
+
+function generateGitbook() {
+    var exec = require('child_process').exec;
+    var cmd  = './node_modules/.bin/gitbook build ./txt ./gh-pages';
+
+    exec(cmd, function(error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        console.log(error);
+    });
+}
