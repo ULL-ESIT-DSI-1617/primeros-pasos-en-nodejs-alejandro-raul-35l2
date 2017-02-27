995d39eb1a70cbf714cf333bd3f6336e28209917
Raul Martin <alu0100769579@ull.edu.es>
2017-02-27 19:51:46 +0000

    Modificando gulpfile #6

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
 const shell = require('gulp-shell');
 
-gulp.task('deploy', function() {
-  return gulp.src('./gh-pages/**/*')
-    .pipe(ghPages())
+gulp.task('serve',  function() {
+  return gulp.src('').pipe(shell([
+    'node serve.js'
+  ]));
 });
 
-gulp.task('serve', shell.task([
-  'gitbook serve --port ' + process.env.PORT
+gulp.task('build', shell.task([
+  'npm run build'
 ]));
 
-gulp.task('build', shell.task([
-  'gitbook build ./txt gh-pages'
+gulp.task('deploy', shell.task([
+  'npm run deploy'
 ]));
+
